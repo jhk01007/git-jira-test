00182d50fc69331aa51bdc12fe6b0dcaef2e1395
jhk01007 <jhk001007@naver.com>
2024-11-12 10:54:06 +0900

    refactor: HomeController에 /home api 수정

--- src/main/java/hello/gitjiratest/HomeController.java
+++ src/main/java/hello/gitjiratest/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/home")
     public String home() {
-        return "home";
+        return "home3";
     }
 }
