b2061f0807fb6ed43049128ff8e071bf545a10cd
jhk01007 <jhk001007@naver.com>
2024-11-12 09:22:54 +0900

    first commit

---
+++ src/main/java/hello/gitjiratest/GitJiraTestApplication.java
@@ -0,0 +1,13 @@
+package hello.gitjiratest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitJiraTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitJiraTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/gitjiratest/GitJiraTestApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.gitjiratest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitJiraTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
